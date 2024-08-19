95f3a6cb8e17130d704392cedd370bdbe73810bb
Manoj Pethe <manoj.pethe@gmail.com>
2024-08-19 18:06:17 +0530

    router config added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { FormsModule } from '@angular/forms';
 import { ItemComponent } from './item-list/item/item.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+import { Todo2Component } from './todo2/todo2.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: ItemListComponent},
+  { path: 'todo2', component: Todo2Component},
+  { path: '**', redirectTo: '/home'},
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ItemListComponent,
     ItemComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    Todo2Component
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule
   ],
   providers: [],
